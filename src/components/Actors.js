f262cdba5bdd703807daf96ee150999a35a3e103
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-12-24 03:34:11 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,11 +2,34 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
+
+function renderActors() {
+  return actors.map(act =>{
+     return (<div>
+       <p> <b>Name: {act.name}</b> </p>
+       Movies:<ul>
+        {act.movies.map(movie => {
+          return (
+            <li>{movie}</li>
+          )
+        })
+        
+        }
+        </ul>
+       
+         
+       
+     </div>)
+ })
+}
+
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,9 +4,29 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {renderDirectors()}
     </div>
   );
+}
+
+function renderDirectors() {
+  return (
+     directors.map(director => {
+        return (
+          <div>
+            <p><b>Name: {director.name}</b></p>
+            Movies: <ul>
+              {director.movies.map(movie => {
+                return (
+                  <li>{movie}</li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+     })
+  )
 }
 
 export default Directors
