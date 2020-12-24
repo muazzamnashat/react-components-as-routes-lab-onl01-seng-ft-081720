b44b35a60402bdad07ef1f81b3a86aa431cbdbe3
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-12-24 03:34:03 -0500

    commit tree 2e23ac8f52645ab349820dfa74559d8485ef9503

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,26 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovies()}
     </div>
   );
 };
 
+function renderMovies(){
+  return movies.map(movie =>{
+    return (<div>
+      <h2>Name: {movie.title}</h2>
+      <p>Time: {movie.time}</p>
+      <ul>Genres: 
+          {movie.genres.map(genre =>{
+            return (
+              <li>{genre}</li>
+            )
+          })}
+      </ul>
+    </div>
+    )
+  })
+}
 export default Movies;
